81023283f3f7f2b771004c069fe26b8baa308ede
vaibhavskavdia <vaibhavskavdia@gmail.com>
2026-08-05 03:09:32 +0530

    Use Railway DATABASE_URL

--- shared/configs/database.py
+++ shared/configs/database.py
@@ -1,18 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-
 import os
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql://{os.getenv('POSTGRES_USER')}:"
-    f"{os.getenv('POSTGRES_PASSWORD')}@"
-    f"{os.getenv('POSTGRES_HOST')}:"
-    f"{os.getenv('POSTGRES_PORT')}/"
-    f"{os.getenv('POSTGRES_DB')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
